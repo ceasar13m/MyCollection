0534074e84b87baf3abf3e32ff2dc7482b125bb4
ainur <muharlyamovy@gmail.com>
2018-11-14 17:00:49 +0300

    Initial commit

---
+++ src/main/java/java/Main.java
@@ -0,0 +1,18 @@
+package java;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> stringStack = new Stack<String>();
+        stringStack.push("aaa");
+        stringStack.push("bbb");
+        stringStack.push("ccc");
+
+        System.out.println(stringStack.size());
+        System.out.println(stringStack.pop());
+        System.out.println(stringStack.size());
+        System.out.println(stringStack.back());
+        System.out.println(stringStack.size());
+        stringStack.clear();
+        System.out.println(stringStack.size());
+    }
+}
