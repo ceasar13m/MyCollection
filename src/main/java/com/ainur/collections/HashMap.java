6023adf10ff72eb49585088c4513e5d0a06b8a00
ainur <muharlyamovy@gmail.com>
2018-11-17 17:37:26 +0300

    version with map epta 2.0

--- src/main/java/com/ainur/collections/HashMap.java
+++ src/main/java/com/ainur/collections/HashMap.java
@@ -2,47 +2,87 @@ package com.ainur.collections;
 
 public class HashMap<K, V> implements Map<K, V> {
 
-    private class Entry{
+
+    static class Entry{
         final K key;
         final V value;
         Entry next;
 
-        private Entry(K key, V value) {
+        Entry(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
     final int sizeOfHashMap = 16;
-    private HashMap.Entry[] hashTable = new HashMap.Entry[sizeOfHashMap];
+    private Entry[] hashTable = new Entry[sizeOfHashMap];
+    int size = 0;
 
     private int getHashCode(K key) {
         int h;
-        return key == null ? 0 : (h = key.hashCode()) ^ h >>> 16;
+        return key == null ? 0 : ((h = key.hashCode()) ^ h >>> 16) / sizeOfHashMap;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        int hashCode = getHashCode(key);
+        Entry entry = hashTable[hashCode];
+        while(true) {
+            if(entry.key.equals(key)) {
+                return true;
+            }
+            else if (entry.next == null) {
+                return false;
+            }
+            else {
+                entry = entry.next;
+            }
+        }
     }
 
     @Override
     public boolean containsValue(V value) {
+        for (int i = 0; i < hashTable.length; i++) {
+            Entry entry = hashTable[i];
+            while(true) {
+                if(entry.value.equals(value)) {
+                    return true;
+                }
+                else if(entry.next == null) {
+                    break;
+                }
+                else {
+                    entry = entry.next;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int hashCode = getHashCode(key);
+        Entry entry = hashTable[hashCode];
+        while(true) {
+            if(entry.key.equals(key)) {
+                return entry.value;
+            }
+            else if (entry.next == null) {
+                return null;
+            }
+            else {
+                entry = entry.next;
+            }
+        }
     }
 
     @Override
@@ -52,11 +92,27 @@ public class HashMap<K, V> implements Map<K, V> {
 
         if(hashTable[hashCode] == null) {
             hashTable[hashCode] = entry;
-            return (V) hashTable[hashCode].value;
+            return entry.value;
         }
         else {
-            entry.next = hashTable[hashCode].next;
-            hashTable[hashCode].next = entry;
+            Entry pointer = hashTable[hashCode];
+            Entry prePointer = hashTable[hashCode];
+            while(true) {
+                if(pointer.key.equals(entry.key)) {
+                    entry.next = pointer.next;
+                    prePointer.next = entry;
+                    return entry.value;
+                }
+                else if(pointer.next == null) {
+                    pointer.next = entry;
+                    size++;
+                    return entry.value;
+                }
+                else {
+                    prePointer = pointer;
+                    pointer = pointer.next;
+                }
+            }
         }
     }
 
